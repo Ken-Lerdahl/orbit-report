a4f69788559d1fcff8f12884078cf953bd50f92d
Ken-Lerdahl <ken.lerdahl@gmail.com>
2020-04-04 20:44:10 -0500

    Holy crap... I finished bonus C!!

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -10,79 +10,25 @@ import { getNumberOfCurrencyDigits } from '@angular/common';
 })
 export class OrbitCountsComponent implements OnInit {
   @Input() satellites: Satellite[];
-  communicationCount: number;
-  probeCount: number;
-  positioningCount: number;
-  spaceStationCount: number;
-  telescopeCount: number;
-  spaceDebrisCount: number;
   
+  satelliteTypes: string[] = ['Space Debris', 'Communication', 'Probe', 'Positioning', 'Space Station', 'Telescope']
   
   constructor() { 
     
   }
 
   ngOnInit() {
+    
   }
 
-  countSpaceDebris(): number   {
-    this.spaceDebrisCount = 0;
+  typeCount(nameOfType: string): number {
+    let typeCount = 0;
     for(let i=0; i < this.satellites.length; i++) {
-      if (this.satellites[i].type.toLowerCase() === 'space debris') {
-        this.spaceDebrisCount += 1;
+      if (this.satellites[i].type.toLowerCase() === nameOfType.toLowerCase()) {
+        typeCount += 1;
       }
     }
-    return this.spaceDebrisCount;
-  }
-  
-  countCommunication(): number {
-    this.communicationCount = 0;
-    for(let i=0; i < this.satellites.length; i++) {
-      if (this.satellites[i].type.toLowerCase() === 'communication') {
-        this.communicationCount += 1;
-      }
-    }
-    return this.communicationCount;
-  }
-
-  countProbe(): number {
-    this.probeCount = 0;
-    for(let i=0; i < this.satellites.length; i++) {
-      if (this.satellites[i].type.toLowerCase() === 'probe') {
-        this.probeCount += 1;
-      }
-    }
-    return this.probeCount;
-  }
-
-  countPositioning(): number {
-    this.positioningCount = 0;
-    for(let i=0; i < this.satellites.length; i++) {
-      if (this.satellites[i].type.toLowerCase() === 'positioning') {
-        this.positioningCount += 1;
-      }
-    }
-    return this.positioningCount;
-  }
-
-  countSpaceStation(): number {
-    this.spaceStationCount = 0;
-    for(let i=0; i < this.satellites.length; i++) {
-      if (this.satellites[i].type.toLowerCase() === 'space station') {
-        this.spaceStationCount += 1;
-      }
-    }
-    return this.spaceStationCount;
-  }
-
-  countTelescope(): number {
-    this.telescopeCount = 0;
-    for(let i=0; i < this.satellites.length; i++) {
-      if (this.satellites[i].type.toLowerCase() === 'telescope') {
-        this.telescopeCount += 1;
-      }
-    }
-    return this.telescopeCount;
+    return typeCount
   }
 
 }
